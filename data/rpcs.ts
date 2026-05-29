63f599f0282ef9d9c924eacd6472123659803262
GUPPi <110960330+guppi21@users.noreply.github.com>
2026-05-29 11:29:42 +0530

    Update rpcs.ts

--- data/rpcs.ts
+++ data/rpcs.ts
@@ -2,42 +2,19 @@ import { RpcProvider } from "@/types/rpc";
 
 export const rpcs: RpcProvider[] = [
   {
-    name: "Official Mainnet",
-    provider: "Monad",
-    network: "mainnet",
-    url: "https://rpc.monad.xyz",
-  },
-  {
-    name: "dRPC Mainnet",
-    provider: "dRPC",
-    network: "mainnet",
-    url: "https://monad-mainnet.drpc.org",
-  },
-  {
-    name: "Tenderly Mainnet",
-    provider: "Tenderly",
-    network: "mainnet",
-    url: "https://monad.gateway.tenderly.co",
-  },
-  {
-    name: "Thirdweb Mainnet",
-    provider: "Thirdweb",
-    network: "mainnet",
-    url: "https://143.rpc.thirdweb.com",
-  },
-
-  {
     name: "Official Testnet",
     provider: "Monad",
     network: "testnet",
     url: "https://testnet-rpc.monad.xyz",
   },
+
   {
     name: "Ankr Testnet",
     provider: "Ankr",
     network: "testnet",
     url: "https://rpc.ankr.com/monad_testnet",
   },
+
   {
     name: "dRPC Testnet",
     provider: "dRPC",
